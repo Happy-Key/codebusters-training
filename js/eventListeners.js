133333fb9dde21d2024e5ec2f85e4ce7e784b160
ClownDay <88943993+Happy-Key@users.noreply.github.com>
2025-01-23 23:05:52 -0800

    Fix import?
    Actually added the necessary function to import

--- js/eventListeners.js
+++ js/eventListeners.js
@@ -1,4 +1,4 @@
-import {} from './main.js';
+import { selectTab } from './main.js';
 
 export const createEventListeners = () => {
     document.getElementById('homeTabButton').addEventListener('click',() => selectTab("homeTab"));
