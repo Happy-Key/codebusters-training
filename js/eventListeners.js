c8fe09f87765094c129c911908063ab3c71a8c1f
ClownDay <88943993+Happy-Key@users.noreply.github.com>
2025-01-23 22:55:00 -0800

    Added navigation button functionality

---
+++ js/eventListeners.js
@@ -0,0 +1,10 @@
+import {} from './main.js'
+
+export const createEventListeners = () => {
+    document.getElementById('homeTabButton').addEventListener('click',() => selectTab("homeTab"));
+    document.getElementById('hillTabButton').addEventListener('click',() => selectTab("hillTab"));
+    document.getElementById('morseTabButton').addEventListener('click',() => selectTab("morseTab"));
+
+
+    document.addEventListener("DOMContentLoaded", () => selectTab("homeTab"));
+}
---
+++ js/main.js
@@ -0,0 +1,24 @@
+import { createEventListeners } from "./eventListeners.js";
+
+createEventListeners();
+
+
+
+export function selectTab(name) {
+    hideTabs();
+    showTab(name);
+  }
+  
+  function hideTabs() {
+    let tabs = document.getElementsByClassName("tab-content");
+    for (let i = 0; i < tabs.length; i++) {
+      tabs[i].style.display = "none";
+    }
+  }
+  
+  function showTab(name) {
+    let tabs = document.getElementsByClassName("tab-content");
+    for (let i = 0; i < tabs.length; i++) {
+      if (name == tabs[i].id) { tabs[i].style.display = "flex";}
+    }
+  }
